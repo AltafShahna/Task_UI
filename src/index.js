29d17b3cbe15d2a7601b02bd83c013d2a30fe1a5
AltafShahna <Shahnaaltaf@gmail.com>
2025-03-04 15:24:05 +0300

    added env file

--- src/index.js
+++ src/index.js
@@ -7,24 +7,18 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 import { BrowserRouter } from "react-router-dom";
 import { DataControllerProvider } from "./Context";
 
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <GoogleOAuthProvider clientId="154187564843-q4p5l0plkgs7bah9ad6nk9ovhdr6jhln.apps.googleusercontent.com">
-    <BrowserRouter>
-      <DataControllerProvider>
-        <App />
-      </DataControllerProvider>
-    </BrowserRouter>
-  </GoogleOAuthProvider>
+root.render(
+  <React.StrictMode>
+    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
+      <BrowserRouter>
+        <DataControllerProvider>
+          <App />
+        </DataControllerProvider>
+      </BrowserRouter>
+    </GoogleOAuthProvider>
+  </React.StrictMode>
 );
+
+reportWebVitals();
